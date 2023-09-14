f192cdaea0ad31a5a236c214136842ffa3bc0e8e
andreluizborgess <andreluizborges1995@gmail.com>
2023-09-14 15:36:09 -0300

    hjdsbhbsdj

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -2,9 +2,91 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ServicoFormRequest;
+use App\Models\Servico;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ServicoController extends Controller
 {
-    //
+    public function Servico(ServicoFormRequest $request)
+    {
+        $servicos = Servico::create([
+            'nome' => $request->nome,
+            'descrição' => $request->descrição,
+            'duração' => $request->duração,
+            'preco' => $request->preco,
+        ]);
+        return response()->json([
+            "sucess" => true,
+            "message" => "servico cadastrado com sucesso",
+            "data"    => $servicos
+        ], 200);
+    }
+    public function pesquisaPorNome()
+    {
+        $usuarios = Servico::where('nome', 'like', '%')->get();
+        if (count($usuarios) > 0) {
+            return response()->json([
+                'status' => true,
+                'data' => $usuarios
+            ]);
+        }
+        return response()->json([
+            'status' => false,
+            'message' => 'nao ha resultados para pesquisar'
+        ]);
+    }
+    public function exibirServico(){
+        $usuario = Servico::all();
+        return response()->json([
+'status'=> true,
+'data'=>$usuario
+        ]);
+    }
+
+        public function editar(Request $request){
+        $usuario = Servico::find($request->id);
+
+        if(!isset($usuario)){
+            return response()->json([
+                'status'=> false,
+                'message'=>"serviço nao encontrado"
+            ]);
+        }
+        
+        if(isset($request->nome)){
+            $usuario->nome = $request->nome;
+        }
+        if(isset($request->descricao)){
+            $usuario->descricao = $request->descricao;
+        }
+        if(isset($request->duracao)){
+            $usuario->duracao = $request->duracao;
+        }
+        if(isset($request->preco)){
+            $usuario->preco = $request->preco;
+        }
+        $usuario->update();
+        return response()->json([
+            'status'=>false,
+            'message'=>"serviço atualizado"
+        ]);
 }
+public function excluir($id){
+    $usuario = Servico::find($id);
+    if(!isset($usuario)){
+        return response()->json([
+            'status'=>false,
+            'message'=>"serviço nao encontrado"
+        ]);
+    }
+    $usuario->delete();
+    return response()->json([
+        'status'=>true,
+        'message'=>"servico excluido"
+    ]);
+}
+}
+
+ 
--- app/Http/Requests/ServicoFormRequest.php
+++ app/Http/Requests/ServicoFormRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ServicoFormRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class ServicoFormRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,31 @@ class ServicoFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nome' => 'required|max:90|min:5',
+            'descrição' => 'required|max:200|min:10',
+            'duração' => 'required|integer',
+            'preco' => 'required|decimal:2'
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'sucess' => false,
+            'error' => $validator->errors()
+        ]));
+    }
+    public function messages()
+    {
+        return [
+            'nome.required' => 'o campo nome é obrigatório',
+            'nome.max' => 'o campo deve conter no maximo 80 caracteres',
+            'nome.min' => "o campo deve conter no minimo 5 caracteres",
+            'descricao.required' => 'o campo descricao é obrigatório',
+            'descricao.max' => 'o campo deve conter no maximo 200 caracteres',
+            'descricao.min' => 'o campo deve conter no minimo 10 caracteres',
+            'preco.required' => 'campo obrigatorio',
+            'preco.decimal' => 'o formato de preço é invalido'
+
         ];
     }
 }
